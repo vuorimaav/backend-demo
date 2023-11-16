d89535b104db573463bcb4278135e9381b07db5e
Ville-Veikko Vuorimaa <ville-veikko.vuorimaa@mavericks.fi>
2023-11-16 10:57:15 +0200

    Add jooq generated classes

--- src/main/kotlin/com/vvv/vvdemo/service/BookService.kt
+++ src/main/kotlin/com/vvv/vvdemo/service/BookService.kt
@@ -1,13 +1,24 @@
 package com.vvv.vvdemo.service
 
+import com.vvv.vvdemo.repo.BookRepository
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
+import java.util.*
 
 @Service
 class BookService {
 
-    fun getBookByIsbn(isbn: String): BookDto {
-        return BookDto("New book name", isbn)
+    @Autowired
+    lateinit var bookRepository: BookRepository
+
+    fun getBookByIsbn(isbn: String): Optional<BookDto> {
+        val result = bookRepository.getBookByIsbn(isbn);
+        if (result.isPresent) {
+            val book = result.get()
+            return Optional.of(BookDto(book.value1().orEmpty(), book.value2().orEmpty(), isbn))
+        }
+        return Optional.empty()
     }
 
-    class BookDto(val name: String, val isbn: String)
+    class BookDto(val name: String, val author: String, val isbn: String)
 }
